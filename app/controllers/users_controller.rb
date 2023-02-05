6b54bf4e761e89fe598eab7ab3307f2a61d08ea4
jun218391 <gongzezhun@gmail.com>
2023-02-05 07:45:51 +0000

    [add]いいね機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
-  before_action :is_matching_login_user, only: [:edit, :update]
+  before_action :ensure_correct_user, only: [:edit, :update]
+  # before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -39,10 +39,10 @@ class UsersController < ApplicationController
     end
   end
   
-  def is_matching_login_user
-    user_id = params[:id].to_i
-    unless user_id == current_user.id
-      redirect_to user_path(current_user)
-    end
-  end
+  # def is_matching_login_user
+  #   user_id = params[:id].to_i
+  #   unless user_id == current_user.id
+  #     redirect_to user_path(current_user)
+  #   end
+  # end
 end
