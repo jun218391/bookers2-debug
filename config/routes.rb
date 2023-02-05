f957ed9fc9c22ddbc967a1146b375e7e8cf65d6f
jun218391 <gongzezhun@gmail.com>
2023-02-05 20:06:10 +0000

    [finish]応用課題３

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   devise_for :users #先に書く
   
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
-    resource :book_comments, only: [:create, :destroy]
   end
   
   resources :users, only: [:index,:show,:edit,:update]
